d672342be362f5048f8cb4bb07dc4ea6e8b957e2
njae94 <azuretic94@gmail.com>
2023-12-12 18:32:02 +0900

    스타일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import {Wrapper} from "@/app/rootPage.css";
+import {Left, Right, Signup, Wrapper, Login} from "@/app/rootPage.css";
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-   <div className={Wrapper}>테스트</div>
+   <div className={Wrapper}>
+     <div className={Left}>
+         <Image src="/Logo.svg" width={539} height={444} alt="로고" />
+     </div>
+     <div className={Right}>
+         <h1>지금 일어나고 있는 일</h1>
+         <h2>지금 가입하세요.</h2>
+         <Link href="/i/flow/signup" className={Signup}>계정 만들기</Link>
+         <h3>이미 트위터에 가입하셨나요?</h3>
+         <Link href="/login" className={Login}>로그인</Link>
+     </div>
+   </div>
   )
 }
 
--- src/app/rootPage.css.ts
+++ src/app/rootPage.css.ts
@@ -1,6 +1,64 @@
-import {style} from "@vanilla-extract/css";
+import {globalStyle, style} from "@vanilla-extract/css";
 
 export const Wrapper = style({
     display: "flex",
-    backgroundColor: "red",
+    width: '100dvw',
+    height: '100dvh',
 })
+
+export const Left = style({
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+
+});
+
+globalStyle(`${Left} h1`, {
+    margin: '10px',
+    fontSize: '64px'
+});
+
+globalStyle(`${Left} h2`, {
+    fontSize: '32px',
+    marginBottom: '32px'
+});
+
+globalStyle(`${Left} h3`, {
+    fontSize: '24px',
+    marginBottom: '20px',
+    marginTop: '40px'
+});
+
+export const Right = style([Left]);
+
+export const Signup = style({
+    fontSize: '16px',
+    width: '300px',
+    height: '40px',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(29, 155, 240)',
+
+    ':hover': {
+        backgroundColor: 'rgb(26, 140, 216)'
+    }
+})
+
+export const Login = style({
+    fontSize: '16px',
+    width: '300px',
+    height: '40px',
+    color:' rgb(29, 155, 240)',
+    border: '1px solid rgb(207, 217, 222)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    ':hover': {
+        backgroundColor: 'rgba(29,155,240,0.1)'
+    }
+});
