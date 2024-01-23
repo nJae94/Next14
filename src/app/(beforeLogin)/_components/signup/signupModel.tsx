b1b1b7675c87cb9f589746b90790f5df701364f8
njae94 <azuretic94@gmail.com>
2024-01-23 16:10:33 +0900

    테스트

--- src/app/(beforeLogin)/_components/signup/signupModel.tsx
+++ src/app/(beforeLogin)/_components/signup/signupModel.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
     Modal,
     ModalHeader,
@@ -7,35 +8,33 @@ import {
     InputWrapper, InputLabel, Input, ModalFooter, ActionButton, Error
 } from "@/app/(beforeLogin)/_components/signup/signupModel.css";
 import BackButton from "@/app/(beforeLogin)/_components/BackButton";
-import {useFormState, useFormStatus } from "react-dom";
 import onSubmit from '../../_lib/signup';
+import { useFormState, useFormStatus } from 'react-dom';
 
 
 
-
+function showMessage(message: string | null) {
+    if (message === 'no_id') {
+        return '아이디를 입력하세요.';
+    }
+    if (message === 'no_name') {
+        return '닉네임을 입력하세요.';
+    }
+    if (message === 'no_password') {
+        return '비밀번호를 입력하세요.';
+    }
+    if (message === 'no_image') {
+        return '이미지를 업로드하세요.';
+    }
+    if (message === 'user_exists') {
+        return '이미 사용 중인 아이디입니다.';
+    }
+    return '';
+}
 const SignupModal = () => {
     const [state, formAction] = useFormState(onSubmit, { message: null });
     const { pending } = useFormStatus();
 
-    const showMessage = (message:string | null) => {
-        if (message === 'no_id') {
-            return '아이디를 입력하세요.';
-        }
-        if (message === 'no_name') {
-            return '닉네임을 입력하세요.';
-        }
-        if (message === 'no_password') {
-            return '비밀번호를 입력하세요.';
-        }
-        if (message === 'no_image') {
-            return '이미지를 업로드하세요.';
-        }
-        if (message === 'user_exists') {
-            return '이미 사용 중인 아이디입니다.';
-        }
-        return '';
-    }
-    
     return (
         <div className={Wrapper}>
             <div className={Modal}>
@@ -75,7 +74,7 @@ const SignupModal = () => {
                         </div>
                     </div>
                     <div className={ModalFooter}>
-                        <button className={ActionButton} type='submit'>가입하기</button>
+                        <button type="submit" className={ActionButton} disabled={pending}>가입하기</button>
                         <div className={Error}>{showMessage(state?.message)}</div>
                     </div>
                 </form>
